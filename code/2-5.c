30ac57e8fc26a24ebc5cac80e24c30236bf81bd0
alvin7187 <alvin060718@gmail.com>
2026-05-28 10:07:28 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 3)
+int main(int argc, const char* argv[]){
+    if(argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    int ch;
+    
+    while ((ch = fgetc(fp_src)) != EOF) {
+        fputc(ch, fp_dst);
+    }
+    
+    fclose(fp_src);
+    fclose(fp_dst);
 }
-
